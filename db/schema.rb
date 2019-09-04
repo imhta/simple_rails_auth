29633b6a45e84d582bc2332f2c9d5c88d33c9309
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-09-04 18:26:09 +0500

    add user_id foreign key to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_110728) do
+ActiveRecord::Schema.define(version: 2019_09_04_122003) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
