daf8606ee0630ad3aaa0a83e17e310c58fc953b3
imhta <imhtapm@gmail.com>
2019-09-04 15:46:23 +0530

    generate new toke method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+
+  private
+
+  generate_new_token 
+    SecureRandom.urlsafe_base64
+  end
+  
+
+
 end
