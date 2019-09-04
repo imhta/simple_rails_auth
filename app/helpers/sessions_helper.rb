e7ed9fe097897860450b5dcc18603f8314305aca
imhta <imhtapm@gmail.com>
2019-09-04 16:22:58 +0530

    save cookies method added to session helper to save user id and remember token

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,10 @@ module SessionsHelper
   def log_in user
     session[:user_id] = user.id
   end
+  
+  def save_cookies user
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
 end
