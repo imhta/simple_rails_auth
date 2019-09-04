be6ddbdbd2d7eb67992f8d297ac4218c21fdc308
imhta <imhtapm@gmail.com>
2019-09-04 16:28:14 +0530

    added current user  and current user? helper to session controller

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,4 +8,20 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  def current_user? user
+    user == current_user
+  end
+
+  def current_user
+    if (user.id == session[:user_id])
+        @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(:remember, cookies[:remember_token])
+            log_in user
+            @current_user = user
+        end
+    end
+  end
+  
 end
