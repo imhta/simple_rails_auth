ac80416dc4022a86a388d59e85b251e04d9c7c7c
imhta <imhtapm@gmail.com>
2019-09-04 16:42:09 +0530

    posts controller genereated with new, create, index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   root 'sessions#new'
   get '/login', to: 'sessions#new'
   post '/login',   to: 'sessions#create'
