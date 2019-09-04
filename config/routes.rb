3d5ffd49afc8335a84456b781a0d92a32adaae7f
imhta <imhtapm@gmail.com>
2019-09-04 19:41:13 +0530

    changed posts as index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'posts/new'
   get 'posts/create'
   get 'posts/index'
-  root 'sessions#new'
+  root 'posts#index'
   get '/login', to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
