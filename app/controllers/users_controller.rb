514acb23426c544db53b2a14ea954daae6a1a284
Abdusaid10 <abdusaidabdurasulov@gmail.com>
2019-09-04 14:27:14 +0500

    add actions to session_controller, user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user =User.new
   end
+
+  def create
+  end
+
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'login', to: 'sessions#new'
+  get '/login', to: 'sessions#new'
   post '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get '/signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
