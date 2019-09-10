95be57140350580dc513359185df8f669a8a5093
imhta <imhtapm@gmail.com>
2019-09-10 14:39:37 +0530

    added dependent destory in user for post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  
   before_create :create_remember_token
   before_save { self.email = email.downcase }
-  has_many :posts
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
